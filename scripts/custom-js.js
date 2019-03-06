16125f61925387270308d95c9dc59f423804bcd6
Stephane Lafontaine <stephane@medqigong.ca>
2019-03-06 18:38:03 -0500

    Refactored js to hide louis with conditional

--- scripts/custom-js.js
+++ scripts/custom-js.js
@@ -1,5 +1,28 @@
+// // Remove Louis from bottom of profile pages
+
 var mainPage = document.querySelector('.Main.Main--page');
 
-var louis = document.querySelector('.Main.Main--page').childNodes[1].firstElementChild.textContent;
+if (mainPage !== null) {
+  var louis = mainPage.childNodes[1].firstElementChild.textContent;
+  if (louis.includes("LOUIS-ALEXANDRE")) {mainPage.hidden = true};
+}
 
-if (louis.includes("LOUIS-ALEXANDRE")) {mainPage.hidden = true};
+// // Change text of bands in town ticket buttons
+
+var billetsBtns = document.querySelectorAll('.sqs-tourdates__item');
+// var billetsBtns = document.querySelectorAll('.sqs-tourdates__button');
+// console.log(billetsBtns);
+
+var renameBtns = () => {
+  // billetsBtns.forEach((button, i) => { 
+    // console.log('button');
+    // if (i%2===0) {
+    //   button.text = 'billets'
+    // }else {
+    //   button.text = 'rsvp'
+    // }
+  // });
+};
+
+document.addEventListener("DOMContentLoaded",renameBtns);
+// console.log('test');
