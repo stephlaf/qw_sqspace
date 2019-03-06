ea4bc68ff812eb066cf6b3d1a3af109dd523b827
Stephane Lafontaine <stephane@medqigong.ca>
2019-03-06 16:59:10 -0500

    test

--- scripts/custom-js.js
+++ scripts/custom-js.js
@@ -1,3 +1,3 @@
 console.log('bizoune');
-const louisDisplay = document.getElementById('yui_3_17_2_1_1551896194504_809');
-louisDisplay.hidden = true;
+// const louisDisplay = document.getElementById('yui_3_17_2_1_1551896194504_809');
+// louisDisplay.hidden = true;
