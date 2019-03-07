9858fef2c2bef96aeaa98a60731caccbe45f5738
Stephane Lafontaine <stephane@medqigong.ca>
2019-03-07 13:23:49 -0500

    Added rename buttons function in custom js

--- scripts/custom-js.js
+++ scripts/custom-js.js
@@ -7,22 +7,25 @@ if (mainPage !== null) {
   if (louis.includes("LOUIS-ALEXANDRE")) {mainPage.hidden = true};
 }
 
-// // Change text of bands in town ticket buttons
-
-var billetsBtns = document.querySelectorAll('.sqs-tourdates__item');
-// var billetsBtns = document.querySelectorAll('.sqs-tourdates__button');
-// console.log(billetsBtns);
+// Change text of bands in town ticket buttons
 
 var renameBtns = () => {
-  // billetsBtns.forEach((button, i) => { 
-    // console.log('button');
-    // if (i%2===0) {
-    //   button.text = 'billets'
-    // }else {
-    //   button.text = 'rsvp'
-    // }
-  // });
+  var billetsBtns = document.querySelectorAll('.sqs-tourdates__button');
+  var moreShowsBtn = document.querySelectorAll('.sqs-block-button-element--small.sqs-block-button-element')[1];
+
+  moreShowsBtn.text = "Plus de Spectacles";
+  
+  billetsBtns.forEach((button, i) => { 
+    if (i%2 === 0) {
+      button.text = 'billets';
+    }else {
+      button.text = 'rsvp';
+    }
+  });
+  unScroll();
 };
 
-document.addEventListener("DOMContentLoaded",renameBtns);
-// console.log('test');
+var scroll = () => { window.addEventListener('scroll', renameBtns) };
+var unScroll = () => { window.removeEventListener('scroll', renameBtns) };
+
+document.addEventListener("DOMContentLoaded", scroll);
